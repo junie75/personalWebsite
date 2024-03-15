203e1de88dc1df82f6269a4ff4720d15ca65254f
juni <juniejere6654@gmail.com>
2024-03-15 18:00:24 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const colors = [
+  //   "var(--dark-green)",
+  "var(--light-green)",
+  "var(--dark-sage)",
+  "var(--lighter-brown)",
+  "var(--light-brown)",
+  "var(--dark-brown)",
+
+  "var(--mustard)",
+  "var(--sunset)",
+  "var(--terracotta)",
+  "var(--rust)",
+  "var(--light-sage)",
+  "var(--olive)",
+];
+
+var projects = [
+  {
+    name: "Dino Detectors",
+  },
+  {
+    name: "Rattler's Unite",
+  },
+  {
+    name: "eStore Database",
+  },
+  {
+    name: "Sakura",
+  },
+];
+
+var cardCarousel = document.getElementById("carousel");
+var colorIterator = 0;
+
+for (var project of projects) {
+  var holder = document.createElement("div");
+  holder.classList.add("holder");
+
+  var card = document.createElement("div");
+  card.classList.add("card");
+  holder.appendChild(card);
+
+  if (colorIterator > colors.length) {
+    colorIterator = 0;
+  }
+  card.style.backgroundColor = colors[colorIterator];
+  holder.style.color = colors[colorIterator];
+  colorIterator++;
+  var nameTitle = document.createElement("p");
+  nameTitle.textContent = project.name;
+
+  holder.appendChild(nameTitle);
+  //   var description = document.createElement("p");
+  //   description.textContent = project.description;
+
+  //   card.appendChild(nameTitle);
+  //   card.appendChild(description);
+
+  cardCarousel.appendChild(holder);
+}
